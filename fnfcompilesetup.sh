4be50c5eddbf36d8642f4a204a135c1ccbbe9eaf
toxl <randomlol0315@gmail.com>
2021-07-10 19:24:16 +0800

    added script for setting up haxe for compiling fnf in one go because im lazy
    typo
    
    minor typo

---
+++ fnfcompilesetup.sh
@@ -0,0 +1,17 @@
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup
+haxelib run lime setup flixel
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib install flixel-addons
+haxelib install flixel-ui haxelib install hscript
+haxelib install newgrounds
+haxelib install linc_luajit
+haxelib install actuate
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/aidan63/linc_discord-rpc
+lime rebuild extension-webm linux
