0a1d232484927fdc376972d591236972149c4fe9
toxl <randomlol0315@gmail.com>
2021-07-19 19:50:47 +0800

    added w95fa, changed .conkyrc to w95fa font, made quickstart.sh copy w95fa. also added win95-wallpaper

--- quickstart.sh
+++ quickstart.sh
@@ -22,5 +22,5 @@ else
 fi
 ln -srf ./.bashrc ~
 ln -srf ./.conkyrc ~
-sudo ln -srf I.MingCP-7.01.ttf /usr/share/fonts &&
+sudo ln -srf I.MingCP-7.01.ttf W95FA.otf /usr/share/fonts &&
 echo "run sudo fc-cache -fv to reload font cache because this script can't fucking do it"
